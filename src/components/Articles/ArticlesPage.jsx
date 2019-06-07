e9517fb6517e80c95d5ca30cc3c389abc8c06fbc
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-06-07 21:40:44 +0100

    fixed typo in ArticlesPage

--- src/components/Articles/ArticlesPage.jsx
+++ src/components/Articles/ArticlesPage.jsx
@@ -39,7 +39,7 @@ class ArticlesPage extends React.Component {
           <h2>Articles</h2>
           {loggedInUser && (
             <Link className="ui button blue" to="/addArticle">
-              <i class="icon edit" />
+              <i className="icon edit" />
               Post new article
             </Link>
           )}
