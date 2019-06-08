5a5470088a292bcc45675db90c952125e78ef2e9
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-06-08 12:45:53 +0100

    added style to topics page

--- src/components/Topics/TopicsList.jsx
+++ src/components/Topics/TopicsList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Topics.css';
 import { Link } from '@reach/router';
 
 const TopicsList = ({ topics }) => {
@@ -6,13 +7,11 @@ const TopicsList = ({ topics }) => {
     <>
       {topics.map((topic, i) => {
         return (
-          <li key={`topic${i}`}>
-            <h3>
-              <Link className="entry" to={`/topics/${topic.slug}`}>
-                {topic.slug}
-              </Link>
-            </h3>
-            <p>{topic.description}</p>
+          <li className="ui segment topic" key={`topic${i}`}>
+            <Link className="entry" to={`/topics/${topic.slug}`}>
+              <h3>{topic.slug}</h3>
+              <p className="topic-description">{topic.description}</p>
+            </Link>
           </li>
         );
       })}
