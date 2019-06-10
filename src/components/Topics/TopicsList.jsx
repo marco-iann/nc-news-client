0007451d73d26edec3f6f72510580707232ea187
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-06-10 14:28:17 +0100

    removed outline when clicking on links

--- src/components/Topics/TopicsList.jsx
+++ src/components/Topics/TopicsList.jsx
@@ -7,8 +7,8 @@ const TopicsList = ({ topics }) => {
     <>
       {topics.map((topic, i) => {
         return (
-          <li className="ui segment topic" key={`topic${i}`}>
-            <Link className="entry" to={`/topics/${topic.slug}`}>
+          <li className="ui segment topic-box" key={`topic${i}`}>
+            <Link className="topic" to={`/topics/${topic.slug}`}>
               <h3>{topic.slug}</h3>
               <p className="topic-description">{topic.description}</p>
             </Link>
