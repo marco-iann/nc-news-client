7e6c3e677c9e56d9bb720e04bb90b9705afbe636
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-06-16 16:11:45 +0100

    add logo to header and modify style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,17 @@ import { Link } from '@reach/router';
 
 const Header = ({ user, logOut }) => {
   return (
-    <div className="header ui container">
-      <h1>Northcoders News</h1>
-      {user && <p className="logged-in-user">You are logged in as {user}</p>}
-      <div>
+    <div className="ui container title">
+      <div className="ui header top-header">
+        <img
+          className="logo"
+          src="https://www.manchesterdigital.com/sites/default/files/learn_to_code_manchester_rw_original_0.png"
+          alt="Northcoders"
+        />
+        <h1>News</h1>
+        {user && <p className="logged-in-user">You are logged in as {user}</p>}
+      </div>
+      <div className="ui header navbar">
         <Link className="link" to="/topics">
           Topics
         </Link>
