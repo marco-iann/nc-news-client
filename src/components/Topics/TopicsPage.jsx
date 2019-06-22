1934db2442f9bff863809e9b0f00ad333d2839b5
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-06-22 21:55:48 +0100

    add error handling to topics page

--- src/components/Topics/TopicsPage.jsx
+++ src/components/Topics/TopicsPage.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import TopicsList from './TopicsList';
 import Loader from '../Loader';
+import Error from '../Error';
 import { getTopics } from '../../api';
 
 class TopicsPage extends React.Component {
-  state = { topics: [] };
+  state = { topics: null, err: null };
 
   componentDidMount() {
-    getTopics().then(topics => this.setState({ topics }));
+    getTopics()
+      .then(topics => this.setState({ topics }))
+      .catch(err => this.setState({ err }));
   }
 
   render() {
-    const { topics } = this.state;
+    const { topics, err } = this.state;
+    const renderedTopics = err ? (
+      <Error err={err} />
+    ) : (
+      <ul>
+        <TopicsList topics={topics} />
+      </ul>
+    );
     return (
       <div className="ui container segment">
         <h2>Topics</h2>
-        {topics.length === 0 ? (
-          <Loader />
-        ) : (
-          <ul>
-            <TopicsList topics={topics} />
-          </ul>
-        )}
+        {topics || err ? renderedTopics : <Loader />}
       </div>
     );
   }
